da340df0bedbc2eccc0cea69e7c177e4d55c452a
Graham <grr.cowie@gmail.com>
2026-04-20 22:32:38 +0100

    Working encoder config witah RGB and desktop switching

--- keyboards/sofle_pico/keymaps/koombot_vial/keymap.c
+++ keyboards/sofle_pico/keymaps/koombot_vial/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_QWERTY] = LAYOUT(
         KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
         KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
-        KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+        KC_TAB,   KC_A,   KC_S,    KC_D,    LSFT_T(KC_F),    KC_G,                          KC_H,    LSFT_T(KC_J),    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                           KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_ENT,      KC_SPC , TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
         ),
@@ -103,9 +103,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] = {ENCODER_CCW_CW(KC_PGUP, KC_PGDN)},
-    [2] = {ENCODER_CCW_CW(_______, _______)},
-    [3] = {ENCODER_CCW_CW(_______, _______)},
+    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP)},
+    [1] = {ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(LWIN(LCTL(KC_LEFT)), LWIN(LCTL(KC_RGHT)))},
+    [2] = {ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)},
+    [3] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)},
 };
 #endif
