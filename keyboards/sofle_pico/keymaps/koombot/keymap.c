b7f86b714eb33687c3e79da07b3963d6d87a9217
koombot <84815933+koombot@users.noreply.github.com>
2026-05-21 12:22:28 +0000

    Add delete key override for backspace

--- keyboards/sofle_pico/keymaps/koombot/keymap.c
+++ keyboards/sofle_pico/keymaps/koombot/keymap.c
@@ -9,6 +9,9 @@ void keyboard_post_init_user(void) {
     }
 }
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t *key_overrides[] = {&delete_key_override};
+
 enum sofle_layers {
     _QWERTY,
     _LOWER,
