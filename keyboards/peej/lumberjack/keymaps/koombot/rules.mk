b99a4c3904260098cadf3d368cc64855666884df
Graham <grr.cowie@gmail.com>
2026-07-04 08:14:43 +0100

    Add koombot keymap skeleton for lumberjack with LTO and layer scaffolding

---
+++ keyboards/peej/lumberjack/keymaps/koombot/rules.mk
@@ -0,0 +1,24 @@
+#Firmware Optimisation
+LTO_ENABLE = yes  # Enable Link Time Optimisation (LTO) for smaller firmware size and faster execution
+BOOTMAGIC_ENABLE = no # Disable Bootmagic to reduce firmware size
+TAP_DANCE_ENABLE = no # Disable Tap Dance to reduce firmware size
+COMBO_ENABLE = no # Disable Combo to reduce firmware size
+MOUSEKEY_ENABLE = no # Disable Mouse Key to reduce firmware size
+CONSOLE_ENABLE = no # Disable Console to reduce firmware size
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+QMK_SETTINGS = yes
+
+TRI_LAYER_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+
+
+
+
+
+
+
+
